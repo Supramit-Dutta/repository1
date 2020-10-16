fede8af86d491203573018381017e77f1b3abe09
SUPRAMIT DUTTA <SUPRAMIT DUTTA@192.168.0.103>
2020-10-16 09:35:31 +0530

    Added DemoClass3 in master

---
+++ DemoProject1/src/com/lti/demo/DemoClass3.java
@@ -0,0 +1,19 @@
+package com.lti.demo;
+
+public class DemoClass3 {
+	int demo3;
+	String demoName3;
+	public int getDemo3() {
+		return demo3;
+	}
+	public void setDemo3(int demo3) {
+		this.demo3 = demo3;
+	}
+	public String getDemoName3() {
+		return demoName3;
+	}
+	public void setDemoName3(String demoName3) {
+		this.demoName3 = demoName3;
+	}
+	
+}
